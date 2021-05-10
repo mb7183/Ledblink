0f41c6b9c0d5c844b1543eb68bc54b9cbdc5ca68
mb7183 <mb7183@nyu.edu>
2021-05-10 00:20:20 -0400

    added third led

--- Ledblink.ino
+++ Ledblink.ino
@@ -1,16 +1,21 @@
 int ledPin = 13;
 int ledPin2 = 14;
+int ledPin3 = 15;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
 }
 
 void loop() {
   digitalWrite(ledPin, HIGH);
   digitalWrite(ledPin2, HIGH);
+  digitalWrite(ledPin3, HIGH);
   delay(500);
   digitalWrite(ledPin, LOW);
   digitalWrite(ledPin2, LOW);
+  digitalWrite(ledPin3, LOW);
   delay(500);
 
 }
